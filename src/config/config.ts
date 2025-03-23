88c79748b72b67bf8b492f0aa4109b47acc06df7
vined <vinedquotation@gmail.com>
2025-03-23 18:57:40 +0100

    Add youtube notifications

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,19 @@ const config = {
   guildId: '917520262797344779',
   advertisingId: '1322175662903005186',
   generalId: '982192297645056040',
+  youtubeId: '',
+  youtubeQueries: [
+    '6b6t.org',
+    '6b6t',
+  ],
+  youtubeIgnoreWords: [
+    '2b2t',
+    '5b5t',
+    '7b7t',
+    '9b9t',
+    'constantiam',
+    '8b8t',
+  ],
   generalMessage: 'Don\'t have a rank? Get lower /tpa & /home cooldowns and white username by [voting](http://6b6t.org/vote)!',
   advertisingMessage: 'To post here you need <@&1268345919003430942> or <@&1325147417322192927> or <@&1325147393372586054> rank from the [6b6t Shop](https://6b6t.org/shop). Ultra ranks are addons on top of the permanent ranks Prime and Elite.'
 };
