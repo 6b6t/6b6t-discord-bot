85dbe87eb9462812da59a78a684ea5564814d085
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-10-10 21:45:07 +0200

    Update mysql-client.ts

--- src/utils/mysql-client.ts
+++ src/utils/mysql-client.ts
@@ -14,7 +14,7 @@ function createMysqlClient() {
     connectionLimit: 10,
     maxIdle: 10,
     idleTimeout: 60000,
-    queueLimit: 30,
+    queueLimit: 0,
     enableKeepAlive: true,
     keepAliveInitialDelay: 0,
   });
