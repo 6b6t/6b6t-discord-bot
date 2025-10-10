e424132d05b335d12d9f6459a58621bdc08d4f78
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-10-10 18:26:04 +0200

    fix: do redis keepalive

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -11,8 +11,15 @@ export async function getRedisClient(): Promise<ClientType> {
   const redisClient = createClient({
     url: process.env.REDIS_URL,
     name: '6b6t-discord-bot',
+    pingInterval: 30000,
   });
   redisClient.on('error', (err) => console.log('Redis Client Error', err));
+  redisClient.on('reconnecting', () => {
+    console.log('Redis reconnecting…');
+  });
+  redisClient.on('end', () => {
+    console.log('Redis connection ended');
+  });
 
   // Connect to Redis
   await redisClient.connect();
