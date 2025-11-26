b52cbeab04b58a7f96955fdfd733c815156c144a
qbasty <58854397+qbasty@users.noreply.github.com>
2025-11-26 22:45:08 +0100

    Update shop link in command response

--- src/commands/shop.ts
+++ src/commands/shop.ts
@@ -8,7 +8,8 @@ const ShopCommand: Command = {
 
   async execute(interaction: CommandInteraction) {
     await interaction.reply(
-      `You can support 6b6t financially and get benefits like more homes, lower /tpa and /home cooldowns, access to commands like /hat, /balloons, /chatcolor and much more at the [6b6t Shop](<https://www.6b6t.org/?utm_source=discord&utm_medium=discord_bot_command&utm_campaign=discord_bot_command_shop>).`,
+      `You can support 6b6t financially and get benefits like more homes, lower /tpa and /home cooldowns, access to commands like /hat, /balloons, /chatcolor and much more at the [6b6t Shop](<https://www.6b6t.org/shop?utm_source=discord&utm_medium=discord_channel&utm_campaign=evergreen_shop&utm_content=merch_bot&lang=en
+>).`,
     );
   },
 };
